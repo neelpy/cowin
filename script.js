5ccd0cad1d38a5b0616e8d1f2849b07a9dfb57ec
Neel Patel <neelpatel224@gmail.com>
2021-05-01 22:52:01 +0530

    Select state and district

--- script.js
+++ script.js
@@ -1,18 +1,96 @@
 var dateField = document.getElementById('date');
-var age18 = document.getElementById('18');
+var age18 = document.getElementById('18+');
+var date = document.getElementById('date');
 var results = document.getElementById('results');
+var state = document.getElementById('state');
+var district = document.getElementById('district');
+
+var states = {
+    "Andaman and Nicobar Islands": 1,
+    "Andhra Pradesh": 2,
+    "Arunachal Pradesh": 3,
+    "Assam": 4,
+    "Bihar": 5,
+    "Chandigarh": 6,
+    "Chhattisgarh": 7,
+    "Dadra and Nagar Haveli": 8,
+    "Daman and Diu": 37,
+    "Delhi": 9,
+    "Goa": 10,
+    "Gujarat": 11,
+    "Haryana": 12,
+    "Himachal Pradesh": 13,
+    "Jammu and Kashmir": 14,
+    "Jharkhand": 15,
+    "Karnataka": 16,
+    "Kerala": 17,
+    "Ladakh": 18,
+    "Lakshadweep": 19,
+    "Madhya Pradesh": 20,
+    "Maharashtra": 21,
+    "Manipur": 22,
+    "Meghalaya": 23,
+    "Mizoram": 24,
+    "Nagaland": 25,
+    "Odisha": 26,
+    "Puducherry": 27,
+    "Punjab": 28,
+    "Rajasthan": 29,
+    "Sikkim": 30,
+    "Tamil Nadu": 31,
+    "Telangana": 32,
+    "Tripura": 33,
+    "Uttar Pradesh": 34,
+    "Uttarakhand": 35,
+    "West Bengal": 36
+}
+var districts = {};
 
 function getDate() {
     const d = dateField.value;
-    return (d.length === 1 ? '0' : '') +  d + '-05-2021'
+    return d.split('-').reverse().join('-')
 }
 function getAge() {
     return age18.checked ? 18 : 45;
 }
+function setStates() {
+    for (const s in states) {
+        var opt = document.createElement('option');
+        opt.value = states[s]
+        opt.text = s
+        state.appendChild(opt)
+    }
+}
+function setDistricts(d = null) {
+    var req = new XMLHttpRequest();
+    const url = `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${state.value}`
+    req.open('GET', url, true);
+    req.responseType = 'json';
+    req.onload = function() {
+        var status = req.status;
+        if (status === 200) {
+            districts = req.response.districts;
+            const def = district.children[0]
+            district.innerHTML = '';
+            district.appendChild(def)
+            for (const d of districts) {
+                var opt = document.createElement('option');
+                opt.value = d.district_id;
+                opt.text = d.district_name;
+                district.appendChild(opt)
+            }
+            if (d) district.value = d;
+        }
+      };
+    req.send();
+}
+function getDistrictId() {
+    return district.value;
+}
 
 function get(callback) {
     var req = new XMLHttpRequest();
-    const district_id = '770';
+    const district_id = district.value;
     const date = getDate();
     const url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${district_id}&date=${date}`;
     req.open('GET', url, true);
@@ -35,6 +113,7 @@ function check() {
         const available = res.centers.filter(center => {
             return center.sessions.some(s => (s.available_capacity > 0 && s.min_age_limit === age))
         })
+        console.log(available);
         const template = center => `
             <div class="center" style="border: 1px solid black">
                 <b>${center.name}, Pincode: ${center.pincode}</b><br>
@@ -46,4 +125,24 @@ function check() {
         else
             results.innerHTML = available.map(c => template(c)).join(' ')
     })
+}
+
+state.onchange = () => {
+    localStorage.setItem('state', state.value)
+    setDistricts()
+}
+
+district.onchange = () => {
+    localStorage.setItem('district', district.value)
+}
+
+window.onload = () => {
+    const s = localStorage.getItem('state')
+    const d = localStorage.getItem('district')
+    setStates();
+    if (s) {
+        state.value = s;
+        setDistricts(d);
+    }
+    date.valueAsDate = new Date();
 }
